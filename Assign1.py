c8212477606f2ff81ccccc5916d4992d0d345ff0
prattaysanyal <40462781+prattaysanyal@users.noreply.github.com>
2018-06-21 19:07:11 +0530

    Add files via upload

---
+++ Assign1.py
@@ -0,0 +1,5 @@
+'''Time-tuple accepts an instant expressed in local time and
+returns a string representing the instant as specified by string fmt.
+Parses str according to format string fmt and returns the instant in
+time-tuple format. Returns the current time instant, a floating-point
+number of seconds since the epoch.'''
